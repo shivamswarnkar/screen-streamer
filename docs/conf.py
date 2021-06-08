4529c76d90bc2a85baa89ab661095edbd5cfd556
shivamswarnkar <ss8464@nyu.edu>
2021-06-08 21:49:41 +0530

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,13 +13,11 @@
 import os
 import sys
 
-sys.path.insert(
-    0,
-    os.path.abspath('/Users/shivamswarnkar/Desktop/projects/screen-streamer'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'screener'
+project = 'Screen Streamer'
 copyright = '2021, Shivam Swarnkar'
 author = 'Shivam Swarnkar'
 
@@ -39,7 +37,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,17 +1,16 @@
-.. Screener documentation master file, created by
-   sphinx-quickstart on Tue Jun  8 21:12:40 2021.
+.. Screen Streamer documentation master file, created by
+   sphinx-quickstart on Tue Jun  8 21:45:37 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Screener's documentation!
-====================================
+Welcome to Screen Streamer's documentation!
+===========================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    modules
-
 
 
 Indices and tables
